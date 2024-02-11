31fb71d0f27fc2a5b724569b2cc0d0095a4e2c5b
humayunkabir506034 <humayunkabir506034@gmail.com>
2024-02-12 00:55:16 +0600

    complete

--- js/events.js
+++ js/events.js
@@ -1,16 +1,33 @@
-//  option two function
+//  option 2
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
-// option three function
+// option 3
 const makeBlueButton = document.getElementById("make-blue");
 makeBlueButton.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 
-// option three onother function
+// option 3 onother
 const purpleButton = document.getElementById("make-purple");
 purpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple';
 }
+
+// option 4
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink)
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+// option 4 another
+const greenButton = document.getElementById('make-green');
+greenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green'
+})
+
+// option 4 final
+document.getElementById('make-goldenrod').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenrod';
+})
